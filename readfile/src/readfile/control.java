6b9532421a551637c793e90b0846379212a4b017
MichaelDIT <c16464644@mydit.ie>
2018-04-02 04:52:26 +0100

    first commit
    "Readf" is the working file for openig and printing file contents.

---
+++ readfile/src/readfile/control.java
@@ -0,0 +1,16 @@
+
+package readfile;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class control 
+{
+    public static void main(String [] args)
+    {
+    	
+    
+    
+    }//close main
+    
+}//close class
