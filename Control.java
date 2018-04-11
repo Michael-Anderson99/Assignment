26257b24641a47a6e01d23f61292882116d145fb
MichaelDIT <c16464644@mydit.ie>
2018-04-11 13:28:01 +0100

    commit 2
    checks word length, sentence length

---
+++ Control.java
@@ -0,0 +1,10 @@
+
+public class Control 
+{
+
+	public static void main(String[] args) 
+	{
+		Readfile rf = new Readfile("C:\\Users\\Michael\\Documents\\College\\Y2 S2\\OOP\\Assignment\\src");
+	}
+
+}
